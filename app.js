ea39230a470d2331ab6ffb483fb3b45e7754e092
nicor528 <nicolasriquelme528@gmail.com>
2023-08-11 01:06:57 -0300

    asd

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const singUp = require("./routes/SingUp");
+const singIn = require("./routes/SingIn");
 
 const app = express();
 app.use(express.static('public'));
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({limit: '10mb',  extended: false }));
 app.use(singUp);
+app.use(singIn);
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
