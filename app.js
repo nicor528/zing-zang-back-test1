e79385843aaf4e074e055f9fab438e87663bb752
nicor528 <nicolasriquelme528@gmail.com>
2023-08-11 00:51:01 -0300

    asd

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-
+console.log("no")
 
 const PORT = process.env.PORT || 4242;
 app.listen(PORT, () => console.log("server up en", PORT));
